8455ee5713c756aefc7c4b67d77bd9e5dc4cab39
kimkyeongbeom <bibix321@naver.com>
2022-07-05 11:34:01 +0900

    DOM css add

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const hello = 'hello!';
-console.log(hello)
+console.log(hello);
+
+let $messageBox = document.querySelector('.msgBox');
+$messageBox.style.width = '100px';
+$messageBox.style.height = "100px";
+$messageBox.style.backgroundColor = "green";
+$messageBox.innerHTML = '채팅창입니다'
