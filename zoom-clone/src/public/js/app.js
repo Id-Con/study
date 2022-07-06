8e6eb62c28e3120567c7eb5906b3de10a5e81111
JwaSangHeon <whk4051@naver.com>
2022-07-06 17:45:04 +0900

    websocket 활용

--- zoom-clone/src/public/js/app.js
+++ zoom-clone/src/public/js/app.js
@@ -1,1 +1,24 @@
-hell1234;
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New Message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ");
+});
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+};
+
+messageForm.addEventListener("submit", handleSubmit);
